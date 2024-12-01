174bc0fd4538ee96962db1ad5a4e7311ce7fd137
legacychimera247 <mad_fer_it@vivaldi.net>
2024-12-01 14:25:24 +0100

    Add files via upload

---
+++ translations/harbour-sshazam-it.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Errors</name>
+    <message>
+        <source>Python error: %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Recognition Result</source>
+        <translation>Traccia riconosciuta</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Cronologia</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Reload history</source>
+        <translation>Ricarica cronologia</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Could not remove record. Please try again. History was outdated</source>
+        <translation>Non posso rimuovere l&apos;album. Per favore prova di nuovo. La cronologia non era aggiornata</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Recognition time</source>
+        <translation>Durata riconoscimento</translation>
+    </message>
+    <message>
+        <source>Rate</source>
+        <translation>Frequenza</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Elimina cronologia</translation>
+    </message>
+    <message>
+        <source>Reset settings</source>
+        <translation>Resetta impostazioni</translation>
+    </message>
+    <message>
+        <source>Show info messages in notifications</source>
+        <translation>Mostra messaggi d&apos;informazione nelle notifiche</translation>
+    </message>
+</context>
+</TS>
