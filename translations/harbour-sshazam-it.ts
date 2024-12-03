7aaccbb8ac3b5491b36caf01769bc83503721812
legacychimera247 <mad_fer_it@vivaldi.net>
2024-12-03 23:14:30 +0100

    Add files via upload

---
+++ translations/harbour-sshazam-it.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Errors</name>
+    <message>
+        <source>Python error: %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Recognition Result</source>
+        <translation>Traccia riconosciuta</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Cronologia</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Reload history</source>
+        <translation>Ricarica cronologia</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Could not remove record. Please try again. History was outdated</source>
+        <translation>Non posso rimuovere l&apos;album. Per favore prova di nuovo. La cronologia non era aggiornata</translation>
+    </message>
+    <message>
+        <source>Recording</source>
+        <translation>Sto registrando</translation>
+    </message>
+    <message>
+        <source>Processing</source>
+        <translation>Sto processando</translation>
+    </message>
+    <message>
+        <source>Almost done</source>
+        <translation>Quasi fatto</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Caricamento</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Recognition time</source>
+        <translation>Durata riconoscimento</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Elimina cronologia</translation>
+    </message>
+    <message>
+        <source>Reset settings</source>
+        <translation>Resetta impostazioni</translation>
+    </message>
+    <message>
+        <source>Show info messages in notifications</source>
+        <translation>Mostra messaggi d&apos;informazione nelle notifiche</translation>
+    </message>
+    <message>
+        <source>Recognition</source>
+        <translation>Riconoscimento</translation>
+    </message>
+    <message>
+        <source>Sample rate</source>
+        <translation>Frequenza campionamento</translation>
+    </message>
+    <message>
+        <source>Debugging</source>
+        <translation>Debugging</translation>
+    </message>
+    <message>
+        <source>Networking</source>
+        <translation>Rete</translation>
+    </message>
+    <message>
+        <source>Login page always uses the global proxy regardless of these settings. Attachments, avatars and other static elements may not use proxy at all. Restart the app to apply</source>
+        <translation>La pagina di accesso usa sempre le impostazioni globali del proxy a discapito di queste impostazioni. Allegati, avatar e altri elementi statici potrebbero non usare il proxy del tutto. Riavvia l&apos;app per applicare</translation>
+    </message>
+    <message>
+        <source>Proxy</source>
+        <translation>Proxy</translation>
+    </message>
+    <message>
+        <source>global proxy</source>
+        <translation>proxy globale</translation>
+    </message>
+    <message>
+        <source>disable</source>
+        <translation>disabilita</translation>
+    </message>
+    <message>
+        <source>custom</source>
+        <translation>personalizzato</translation>
+    </message>
+    <message>
+        <source>HTTP proxy address</source>
+        <translation>Indirizzo proxy HTTP</translation>
+    </message>
+    <message>
+        <source>Specify port by semicolon, if required</source>
+        <translation>Specifica la porta con un punto e virgola, se necessario</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Memoria</translation>
+    </message>
+    <message>
+        <source>Shazam language</source>
+        <translation>Lingua di Shazam</translation>
+    </message>
+    <message>
+        <source>Example: en-US. Leave empty to use system language</source>
+        <translation>Esempio: it-IT. Lascia vuoto per usare la lingua di sistema</translation>
+    </message>
+</context>
+</TS>
