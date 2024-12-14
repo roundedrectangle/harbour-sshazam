8c32677a75b6b7f04acffabcf6917d3261cd9b1c
legacychimera247 <mad_fer_it@vivaldi.net>
2024-12-14 14:25:21 +0100

    Add files via upload

---
+++ translations/harbour-sshazam-it.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Shazam for SailfishOS</source>
+        <translation>Shazam per SailfishOS</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traduzioni</translation>
+    </message>
+    <message>
+        <source>Italian</source>
+        <translation>Italiano</translation>
+    </message>
+</context>
+<context>
+    <name>Errors</name>
+    <message>
+        <source>Couldn&apos;t reload history: app is not initialized!</source>
+        <translation>Non sono riuscito a ricaricare la cronologia: l&apos;app non è inizializzata!</translation>
+    </message>
+    <message>
+        <source>Python error: %1</source>
+        <translation>Errore python: %1</translation>
+    </message>
+    <message>
+        <source>History was unable to load: %1</source>
+        <translation>Impossibile caricare la cronologia: %1</translation>
+    </message>
+    <message>
+        <source>History contained invalid JSON: %1</source>
+        <translation>La cronologia conteneva  un file JSON invalido: %1</translation>
+    </message>
+    <message>
+        <source>Insufficient permissions: %1</source>
+        <translation>Permessi insufficienti: %1</translation>
+    </message>
+    <message>
+        <source>History is not a list or tuple</source>
+        <translation>La cronologia non è una lista o un insieme di valori</translation>
+    </message>
+    <message>
+        <source>History was outdated. Latest length: %1, previously loaded length: %2</source>
+        <translation>La cronologia non era aggiornata. Ultimo aggiornamento: %1, aggiornamento precedente: %2</translation>
+    </message>
+    <message>
+        <source>Could not remove record. Please try again</source>
+        <translation>Non posso eliminare lo storico. Per favore prova di nuovo</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Recognition Result</source>
+        <translation>Traccia riconosciuta</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Cronologia</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Reload history</source>
+        <translation>Ricarica cronologia</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Recording</source>
+        <translation>Sto registrando</translation>
+    </message>
+    <message>
+        <source>Processing</source>
+        <translation>Sto processando</translation>
+    </message>
+    <message>
+        <source>Almost done</source>
+        <translation>Quasi fatto</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Caricamento</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>LicenseListPart</name>
+    <message>
+        <source>License text</source>
+        <translation>Testo di licenza</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About</name>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versione %1</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Sviluppo</translation>
+    </message>
+    <message>
+        <source>show contributors</source>
+        <translation>mostra contributori</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>Pagina iniziale</translation>
+    </message>
+    <message>
+        <source>Changelog</source>
+        <translation>Cambiamenti</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traduzioni</translation>
+    </message>
+    <message>
+        <source>Source Code</source>
+        <translation>Codice sorgente</translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation>Donazioni</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licenza</translation>
+    </message>
+    <message numerus="yes">
+        <source>show license(s)</source>
+        <translation>
+            <numerusform>mostra licenza</numerusform>
+            <numerusform>mostra licenze</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>show details</source>
+        <translation>mostra dettagli</translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <translation>Contributori</translation>
+    </message>
+    <message>
+        <source>Acknowledgements</source>
+        <translation>Informazioni</translation>
+    </message>
+    <message>
+        <source>Thank you!</source>
+        <translation>Grazie mille!</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Dettagli</translation>
+    </message>
+    <message>
+        <source>Download license texts</source>
+        <translation>Scarica testi di licenza</translation>
+    </message>
+    <message numerus="yes">
+        <source>License(s)</source>
+        <translation>
+            <numerusform>Licenza</numerusform>
+            <numerusform>Licenze</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Note: please check the source code for most accurate information.</source>
+        <translation>Nota: controlla il codice sorgente per avere le informazioni più accurate.</translation>
+    </message>
+    <message>
+        <source>External Link</source>
+        <translation>Collegamento esterno</translation>
+    </message>
+    <message>
+        <source>Open in browser</source>
+        <translation>Apri nel browser</translation>
+    </message>
+    <message>
+        <source>Copied to clipboard: %1</source>
+        <translation>Copiato negli appunti %1</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Copia negli appunti</translation>
+    </message>
+    <message>
+        <source>Please refer to &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>Per favore fai riferimento a &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>News</source>
+        <translation>Novità</translation>
+    </message>
+    <message>
+        <source>Changes since version %1</source>
+        <translation>Cambiamenti dalla versione %1</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About.Common</name>
+    <message>
+        <source>If you want to support my work, you can buy me a cup of coffee.</source>
+        <translation>Se vuoi supportare il mio lavoro puoi offrirmi una tazza di caffè.</translation>
+    </message>
+    <message>
+        <source>You can support this project by contributing, or by donating using any of these services.</source>
+        <translation>Puoi supportare questo progetto contribuendo o donando con uno di questi servizi.</translation>
+    </message>
+    <message>
+        <source>Your contributions to translations or code would be most welcome.</source>
+        <translation>Le tue contribuzioni per le traduzioni o per il codice sono le benvenute.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Recognition time</source>
+        <translation>Durata riconoscimento</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Elimina cronologia</translation>
+    </message>
+    <message>
+        <source>Reset settings</source>
+        <translation>Resetta impostazioni</translation>
+    </message>
+    <message>
+        <source>Show info messages in notifications</source>
+        <translation>Mostra messaggi d&apos;informazione nelle notifiche</translation>
+    </message>
+    <message>
+        <source>Recognition</source>
+        <translation>Riconoscimento</translation>
+    </message>
+    <message>
+        <source>Sample rate</source>
+        <translation>Frequenza campionamento</translation>
+    </message>
+    <message>
+        <source>Debugging</source>
+        <translation>Debugging</translation>
+    </message>
+    <message>
+        <source>Networking</source>
+        <translation>Rete</translation>
+    </message>
+    <message>
+        <source>Proxy</source>
+        <translation>Proxy</translation>
+    </message>
+    <message>
+        <source>global proxy</source>
+        <translation>proxy globale</translation>
+    </message>
+    <message>
+        <source>disable</source>
+        <translation>disabilita</translation>
+    </message>
+    <message>
+        <source>custom</source>
+        <translation>personalizzato</translation>
+    </message>
+    <message>
+        <source>HTTP proxy address</source>
+        <translation>Indirizzo proxy HTTP</translation>
+    </message>
+    <message>
+        <source>Specify port by semicolon, if required</source>
+        <translation>Specifica la porta con un punto e virgola, se necessario</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Memoria</translation>
+    </message>
+    <message>
+        <source>Shazam language</source>
+        <translation>Lingua di Shazam</translation>
+    </message>
+    <message>
+        <source>Example: en-US. Leave empty to use system language</source>
+        <translation>Esempio: it-IT. Lascia vuoto per usare la lingua di sistema</translation>
+    </message>
+    <message>
+        <source>Album arts and other static elements may not use proxy at all. App restart might be required</source>
+        <translation>Copertine ed altri elementi statici potrebbero non usare il proxy del tutto. Un riavvio dell&apos;app potrebbe essere richiesto</translation>
+    </message>
+    <message>
+        <source>Select items to backup</source>
+        <translation>Scegli oggetti da includere nel backup</translation>
+    </message>
+    <message>
+        <source>Recognition history</source>
+        <translation>Cronologia riconoscimenti</translation>
+    </message>
+    <message>
+        <source>Basic settings</source>
+        <translation>Impostazioni di base</translation>
+    </message>
+    <message>
+        <source>Include Recognition section</source>
+        <translation>Includi sezione riconoscimenti</translation>
+    </message>
+    <message>
+        <source>Other settings</source>
+        <translation>Altre impostazioni</translation>
+    </message>
+    <message>
+        <source>Include Debugging and Networking sections</source>
+        <translation>Includi sezioni Debugging e Rete</translation>
+    </message>
+    <message>
+        <source>Unknown error: %1</source>
+        <translation>Errore sconosciuto: %1</translation>
+    </message>
+    <message>
+        <source>Insufficient permissions</source>
+        <translation>Permessi insufficienti</translation>
+    </message>
+    <message>
+        <source>Export in progress</source>
+        <translation>Esportazione in corso</translation>
+    </message>
+    <message>
+        <source>An error occured</source>
+        <translation>Si è verificato un errore</translation>
+    </message>
+    <message>
+        <source>Select items to import</source>
+        <translation>Scegli oggetti da importare</translation>
+    </message>
+    <message>
+        <source>Import in progress</source>
+        <translation>Importazione in corso</translation>
+    </message>
+    <message>
+        <source>All items you select will be overwritten! If you select recognition history your current history will be overwritten!</source>
+        <translation>Tutti gli oggetti selezionati saranno sovrascritti! Se hai scelto la cronologia dei riconoscimenti la tua cronologia attuale verrà sovrascritta!</translation>
+    </message>
+    <message>
+        <source>Export backup</source>
+        <translation>Esporta backup</translation>
+    </message>
+    <message>
+        <source>Import backup</source>
+        <translation>Importa backup</translation>
+    </message>
+    <message>
+        <source>Rebuild history</source>
+        <translation>Ricostruisci cronologia</translation>
+    </message>
+    <message>
+        <source>History cleared</source>
+        <translation>Cronologia eliminata</translation>
+    </message>
+    <message>
+        <source>Settings reset</source>
+        <comment>Past tense</comment>
+        <translation>Reset impostazioni</translation>
+    </message>
+    <message>
+        <source>History rebuilt</source>
+        <translation>Cronologia ricostruita</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-sshazam</name>
+    <message>
+        <source>Migration complete. If you see no errors it means it was succsessful</source>
+        <translation>Migrazione completata. Se non vedi errori vuol dire che il processo ha avuto successo</translation>
+    </message>
+    <message>
+        <source>Legacy history system detected. Attempting to migrate</source>
+        <translation>Rilevato vecchio sistema. Cerco di effettuare la migrazione</translation>
+    </message>
+</context>
+</TS>
