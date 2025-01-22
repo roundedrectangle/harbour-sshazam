6fe623140fa2e7da7486b753c270e251b98e4e29
roundedrectangle <kozelderezel12345678@gmail.com>
2025-01-22 19:24:45 +0300

    Bump to 1.0.3

--- translations/harbour-sshazam-ru.ts
+++ translations/harbour-sshazam-ru.ts
@@ -98,11 +98,11 @@
     <name>General</name>
     <message>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Скопировать</translation>
     </message>
     <message>
         <source>%1 by %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 от %2</translation>
     </message>
 </context>
 <context>
